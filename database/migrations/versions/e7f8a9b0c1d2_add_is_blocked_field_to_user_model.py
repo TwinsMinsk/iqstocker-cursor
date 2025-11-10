868f4db5cc317304548ddeaf0a638b327fa04e43
Oleg Smirnov <72074132+TwinsMinsk@users.noreply.github.com>
2025-11-10 16:24:04 +0300

    исправление миграции

--- database/migrations/versions/e7f8a9b0c1d2_add_is_blocked_field_to_user_model.py
+++ database/migrations/versions/e7f8a9b0c1d2_add_is_blocked_field_to_user_model.py
@@ -1,7 +1,7 @@
 """Add is_blocked field to User model
 
 Revision ID: e7f8a9b0c1d2
-Revises: da56c5c38da4
+Revises: a1b2c3d4e5f7
 Create Date: 2025-11-10 12:30:00.000000
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'e7f8a9b0c1d2'
-down_revision = 'da56c5c38da4'
+down_revision = 'a1b2c3d4e5f7'
 branch_labels = None
 depends_on = None
 
